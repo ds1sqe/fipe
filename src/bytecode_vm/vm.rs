f6e53a07a3c830d8629f477ae94548cb341a0511
ds1sqe <ds1sqe@mensakorea.org>
2024-05-01 14:55:23 +0900

    working on call of function (vm)

--- src/bytecode_vm/vm.rs
+++ src/bytecode_vm/vm.rs
@@ -1,40 +1,64 @@
+use self::frame::Frame;
+
 use super::bytecode::{
     instruction::Instruction, instructions::Instructions, Bytecode,
 };
 use crate::object::{
-    Array, Bool, Int, Object, ObjectTrait, ObjectType, StringObject,
+    Array, Bool, CompiledFunction, Int, Object, ObjectTrait, ObjectType,
+    StringObject,
 };
+
+mod frame;
 
 const GLOBAL_SIZE: usize = 1 << 8;
 
+const FRAME_SIZE: usize = 1 << 10;
+
 pub struct VM {
     stack: Vec<Object>,
+    /// stack pointer
+    sp: usize,
+
     constants: Vec<Object>,
     global: Vec<Object>,
-    instructions: Instructions,
 
-    ic: usize,
+    frames: Vec<Frame>,
+    /// frame pointer
+    fp: usize,
 }
 
 impl VM {
     pub fn new(code: Bytecode) -> Self {
+        let main_func = CompiledFunction {
+            instructions: code.instructions,
+            local_len: 0,
+            arg_len: 0,
+        };
+        let main_frame = Frame::new(main_func, 0);
+
+        let frames = vec![main_frame];
+
         Self {
             stack: Vec::new(),
+            sp: 0,
+
             constants: code.constants,
-            instructions: code.instructions,
             global: Vec::with_capacity(GLOBAL_SIZE),
-            ic: 0,
+
+            frames,
+            fp: 0,
         }
     }
 
     pub fn run_single(&mut self) {
-        let ins = self.instructions.read_instruction(self.ic);
+        let ins = self.current_frame().rext_instruction();
         match &ins {
             Instruction::PUSH => todo!(),
             Instruction::POP => todo!(),
             Instruction::CONST { idx } => {
                 // load constants into stack
-                self.stack.push(self.constants[*idx].clone())
+                self.stack.push(self.constants[*idx].clone());
+                self.sp += 1;
             }
             Instruction::DEFGLB { idx } => {
                 // define global variable
@@ -43,10 +67,12 @@ impl VM {
                 } else {
                     self.global.push(self.stack.pop().unwrap());
                 }
+                self.sp -= 1;
             }
             Instruction::GETGLB { idx } => {
                 // get global variable
-                self.stack.push(self.global[*idx].clone())
+                self.stack.push(self.global[*idx].clone());
+                self.sp += 1;
             }
 
             Instruction::ADD
@@ -190,7 +216,7 @@ impl VM {
             }
 
             Instruction::JMP { idx } => {
-                self.ic = *idx;
+                self.current_frame_mut().set_ic(*idx);
                 return;
             }
             Instruction::JIS { idx } => {
@@ -198,7 +224,7 @@ impl VM {
                 if sign.get_type() == ObjectType::Bool {
                     let Object::Bool(sign) = sign else {unreachable!()};
                     if sign.value {
-                        self.ic = *idx;
+                        self.current_frame_mut().set_ic(*idx);
                         return;
                     }
                 } else {
@@ -210,7 +236,7 @@ impl VM {
                 if sign.get_type() == ObjectType::Bool {
                     let Object::Bool(sign) = sign else {unreachable!()};
                     if !sign.value {
-                        self.ic = *idx;
+                        self.current_frame_mut().set_ic(*idx);
                         return;
                     }
                 } else {
@@ -253,15 +279,20 @@ impl VM {
                     // emit error
                 }
             }
+            Instruction::CALL { arg_len } => {
+                self.current_frame_mut().add_ic(ins.opcode().length());
+                //
+                return;
+            }
             not_implemented => {
                 panic!("not implemented instruction {:?}", not_implemented);
             }
         }
-        self.ic += ins.opcode().length();
+        self.current_frame_mut().add_ic(ins.opcode().length());
     }
 
     pub fn is_runable(&self) -> bool {
-        self.ic < self.instructions.length()
+        self.current_frame().is_runnable()
     }
 
     pub fn top(&self) -> Option<&Object> {
@@ -270,7 +301,6 @@ impl VM {
 
     pub fn to_string(&self) -> String {
         let mut buf = String::new();
-        buf += &format!("IC : {}\n", self.ic);
         buf += "CONSTS\n";
         for (idx, cons) in self.constants.iter().enumerate() {
             buf += &format!("{:0>6}\t\t", idx);
@@ -278,8 +308,13 @@ impl VM {
             buf += "\n";
         }
 
-        buf += "\nINSTRUCTIONS\n";
-        buf += &self.instructions.to_string_with_highlight(self.ic);
+        buf += "\nFrames\n";
+        for (idx, frame) in self.frames.iter().enumerate() {
+            if idx == self.fp {
+                buf += "Current Frame:\n";
+            }
+            buf += &frame.to_string();
+        }
 
         buf += "\nSTACK\n";
         for (idx, ins) in self.stack.iter().enumerate() {
@@ -301,5 +336,37 @@ impl VM {
         }
 
         buf
+    }
+
+    fn current_frame(&self) -> &Frame {
+        &self.frames[self.fp]
+    }
+    fn current_frame_mut(&mut self) -> &mut Frame {
+        &mut self.frames[self.fp]
+    }
+
+    fn push_frame(&mut self, frame: Frame) {
+        self.fp += 1;
+        self.frames.push(frame)
+    }
+    fn pop_frame(&mut self) -> Frame {
+        self.fp -= 1;
+        self.frames.pop().unwrap()
+    }
+
+    fn call(&mut self, arg_len: usize) {
+        let Object::CompiledFunction(fun) = self.stack[self.sp - arg_len].to_owned() else {
+            unreachable!()
+        };
+
+        if arg_len != fun.arg_len {
+            // emit error
+        }
+        let new_bp = self.sp - arg_len;
+        let local_len = fun.local_len;
+
+        let new_frame = Frame::new(fun, self.sp - arg_len);
+
+        self.sp = new_bp + local_len
     }
 }
